d50e262830c62305d328419bc3ce0a00a634d34e
SuperchillB <honnerbertrand@gmail.com>
2018-01-29 11:32:19 +0100

    sort of finish age pop up

--- js/main.js
+++ js/main.js
@@ -1,21 +1,52 @@
+// AGE CONFIRMATION
+
+// $('#confirmAgeContainer').show();
+// $('#hidingDiv').show();
+
+// $('#confirmAgeBtn').on('click', function() {
+// 	$('#hidingDiv').hide();
+// 	$('#confirmAgeContainer').hide();
+// })
+
+// $('#signUpCancel').on('click', function(){
+// 	$('#hidingDiv').hide();
+// 	$('#confirmAgeContainer').hide();
+// })
+// Problem: code above works but pop up keeps appearing on each refresh
+
+// $(document).ready(function() {
+	let popState = localStorage.getItem('popState');
+    if(popState != 'shown'){
+        $('#confirmAgeContainer').show();
+        $('#hidingDiv').show();
+        // localStorage.setItem('popState','shown')
+    } else {
+    	$('#confirmAgeContainer').hide();
+        $('#hidingDiv').hide();
+    }
+
+    $('#confirmAgeBtn').on('click', function() {
+		$('#hidingDiv').fadeOut('fast');
+		$('#confirmAgeContainer').fadeOut('fast');
+		localStorage.setItem('popState','shown');
+	})
+// });
+// Problem: when refresh, pop up shows for less than a second but it shouldn't be showing at all
+
+
+
 // SIGN UP MODAL
 $('#signUpContainer').hide();
-$('#hidingDiv').hide();
+// $('#hidingDiv').hide();
 
 $('#signUpStartBtn').on('click', function() {
-	$('#hidingDiv').show();
-	$('#signUpContainer').show();
+	$('#hidingDiv').fadeIn('fast');
+	$('#signUpContainer').fadeIn('fast');
 })
 
 $('#signUpCancel').on('click', function(){
-	$('#hidingDiv').hide();
-	$('#signUpContainer').hide();
+	$('#hidingDiv').fadeOut('fast');
+	$('#signUpContainer').fadeOut('fast');
 })
+// ==> IN FORM: CODE OWN REQUIREMENTS 
 
-// $('#signUpBtn').on('click', function(e){
-// 	if (condition) {
-// 		// statement
-// 	}
-// })
-
-// IN FORM: CODE OWN REQUIREMENTS 
