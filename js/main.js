cdd6acee775ae3caddc968b20d6e1e95a4ca9031
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-01-30 16:45:07 +0100

    section featured movies ongoing js

--- js/main.js
+++ js/main.js
@@ -126,3 +126,135 @@ $(document).ready(function(){
       }, 600, 'swing');
     });
 });
+
+
+
+
+//FILMS
+
+//
+// let apikey = "908e84944eac33ec8410b6162aa0f393"
+
+
+
+var titre = document.querySelector('#titre');
+var section = document.querySelector('section');
+
+
+
+
+var requestURL = "https://quentinclaes.github.io/allezcine/movie.json";
+
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var superHeroes = request.response;
+  showFilm(superHeroes);
+}
+
+function showFilm(jsonObj) {
+  let film = jsonObj['results'];
+
+  for(var i = 0; i < 12; i++) {
+
+		let myArticle = document.createElement('article');
+		let myH2 = document.createElement('h5');
+		let myPara1 = document.createElement('p');
+		let myPara2 = document.createElement('p');
+		// let myPara3 = document.createElement('p');
+		// let myList = document.createElement('ul');
+		let myimg = document.createElement('img');
+    myH2.textContent = film[i].title;
+    // myPara1.textContent = 'Overview: ' + film[i].overview;
+    myPara2.textContent = film[i].release_date;
+    // myPara3.textContent = 'Superpowers:';
+    myimg.src ='http://image.tmdb.org/t/p/w185' + film[i].backdrop_path;
+
+		$("#filmMoviesVisible").addClass("filmFlex");
+		myArticle.classList.add("articleBox")
+    myArticle.appendChild(myimg);
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    // myArticle.appendChild(myPara3);
+
+    // myArticle.appendChild(myList);
+
+    // section.appendChild(myArticle);
+		// $("#topTrends").append(myArticle);
+
+		$("#filmMoviesVisible").append(myArticle);
+		// $("#featuredTvSeries").append(myArticle);
+    myArticle.id = film[i].id; // CREATE ID FOR EACH ARTICLE CREATED
+
+
+  }
+
+	for(var i = 12; i < film.length; i++) {
+
+		let myArticle = document.createElement('article');
+		let myH2 = document.createElement('h5');
+		let myPara1 = document.createElement('p');
+		let myPara2 = document.createElement('p');
+		// let myPara3 = document.createElement('p');
+		// let myList = document.createElement('ul');
+		let myimg = document.createElement('img');
+    myH2.textContent = film[i].title;
+    // myPara1.textContent = 'Overview: ' + film[i].overview;
+    myPara2.textContent = film[i].release_date;
+    // myPara3.textContent = 'Superpowers:';
+    myimg.src ='http://image.tmdb.org/t/p/w185' + film[i].backdrop_path;
+
+		$("#filmMoviesInvisible").addClass("filmFlex");
+		myArticle.classList.add("articleBox")
+    myArticle.appendChild(myimg);
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    // myArticle.appendChild(myPara3);
+
+    // myArticle.appendChild(myList);
+
+    // section.appendChild(myArticle);
+		// $("#topTrends").append(myArticle);
+
+		$("#filmMoviesInvisible").append(myArticle);
+		// $("#featuredTvSeries").append(myArticle);
+    myArticle.id = film[i].id; // CREATE ID FOR EACH ARTICLE CREATED
+
+		$("#filmMoviesInvisible").hide();
+
+  }
+
+	$('#moviesButton').on('click', function() {
+		$("#filmMoviesInvisible").show();
+	})
+
+
+
+  $('#btn-action').on('click',function(){
+    let notAction = [];
+
+    for (var i = 0; i < jsonObj['results'].length; i++) {
+      if (jsonObj['results'][i]['genre_ids'].indexOf(28) === -1) {
+        notAction.push(jsonObj['results'][i]);
+      }
+    }
+    console.log(notAction);
+    // console.log(notAction[0].id);
+    // console.log(document.getElementsByTagName('article')[0].id);
+
+		for(let i = 0, length1 = notAction.length; i < length1; i++){
+      for(let j = 0, length1 = document.getElementsByTagName('article').length; j < length1; j++){
+        if (document.getElementsByTagName('article')[j].id == notAction[i].id) {
+           document.getElementsByTagName('article')[j].style.display = 'none';
+        }
+
+      }
+    }
+  })
+
+}
